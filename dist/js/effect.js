8a78b62732857c0019b91a6b7bb0c805303b8df4
rivan-nugraha <piksi.irvan.18402043@gmail.com>
2022-09-02 15:09:52 +0700

    Update, 9 September 2022 :: 03.09 AM

--- dist/js/effect.js
+++ dist/js/effect.js
@@ -2,10 +2,17 @@ window.onscroll = function(){effectFunction()};
 
 function effectFunction(){
     if(document.documentElement.scrollTop > 200 ){
-        document.getElementById("hd").className = "header scroll";
         document.getElementById("ct").className = "content-wrapper slideUp";
     }else{
-        document.getElementById("hd").className = "header";
         document.getElementById("ct").className = "content-wrapper slideDown";
+    }
+}
+
+function naviResponsive(){
+    var x = document.getElementById("navigation");
+    if(x.className === "navi-bar"){
+        x.className += " nav-res";
+    }else{
+        x.className = "navi-bar";
     }
 }
